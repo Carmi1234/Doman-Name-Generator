e92648d2035579e178ef630130271172247dae39
Carmi1234 <carmi.adame@hotmail.com>
2020-07-14 23:50:25 +0000

    Domain Name Generator final

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,27 @@
 /* eslint-disable */
 
 import "../assets/img/rigo-baby.jpg";
-import "../assets/img/4geeks.ico";
-//import 'breathecode-dom'; //DOM override to make JS easier to use
+import "../assets/img/4geeks.ico"; //import 'breathecode-dom'; //DOM override to make JS easier to use
 import "../style/index.scss";
 
 window.onload = function() {
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our", "your"];
+  let adj = ["awesome", "giant", "great"];
+  let noun = ["dog", "flag", "horse"];
+  let ext = [".com", ".us", ".net"];
+  let emptyString = "";
+  let myArray = [];
+  for (let randomPronoun in pronoun)
+    for (let randomAdj in adj)
+      for (let randomNoun in noun)
+        for (let randomExt in ext)
+          emptyString +=
+            "www." +
+            pronoun[randomPronoun] +
+            adj[randomAdj] +
+            noun[randomNoun] +
+            ext[randomExt] +
+            "<br>";
+
+  document.querySelector("#domainlist").innerHTML = emptyString;
 };
